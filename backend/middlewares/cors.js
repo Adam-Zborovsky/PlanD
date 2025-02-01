893f10c1aeab950dcbcdaf8c46725dc89587faf5
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2025-02-01 18:03:55 +0200

    Update CORS middleware to include Render deployment URL

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const corsMiddleware = cors({
 	origin: [
 		"http://127.0.0.1:5500",
-		"www.cardsproject.co.il",
 		"http://localhost:5173",
 		"http://localhost:3000",
+		"https://pland.onrender.com",
 		"http://192.168.1.117:3000",
 	],
 });
