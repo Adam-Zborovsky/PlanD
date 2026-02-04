10b7f27ccaba79a402fffee58a60efc8900b84b2
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2026-02-04 16:03:23 +0200

    Update CORS middleware to include new IP address

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,11 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
 	origin: [
-		"http://127.0.0.1:5500",
-		"http://localhost:5173",
-		"http://localhost:3000",
-		"https://pland.onrender.com",
-		"http://192.168.1.117:3000",
+		"<Add IP Address>",
 	],
 });
 module.exports = corsMiddleware;
